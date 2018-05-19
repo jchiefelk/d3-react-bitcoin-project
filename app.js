58ca98255758915f1595794335f80a2678fd1bac
Jackson Chief Elk Admin <facjcadmin@ITE13576.local>
2018-05-18 21:32:45 -0600

    readme cleanup

--- app.js
+++ app.js
@@ -5,29 +5,17 @@ const app = express();
 let Correlation = require('./correlation');
 var bodyParser = require('body-parser');
 
-
-
-
 // Required for POST Requests
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-
 // Set Port
 app.set('port', (process.env.PORT || 3000));
-
 app.use(express.static(path.resolve(__dirname,'./','bundle')));
-
-
 
 app.post('/api', function(req,res){
 
-  	//console.log(req.body);
   	Correlation.quandl_autocorrelation(req.body)
   		.then((results)=>{
-  			//console.log(results);
 			res.json({autocorrdata: results}); 
 		})
 		.catch((err)=>{
@@ -37,73 +25,35 @@ app.post('/api', function(req,res){
 		})
 
 });
-
-
-/**
-app.get('/api', function(req,res){  
-	fetch('https://www.quandl.com/api/v3/datasets/BCHARTS/BITSTAMPUSD.json?api_key=oaWPkjrfz_aQmyPmE-WT',{
-		method: 'get',
-		mode: 'cors'
-	})
-	.then((response) => typeof response == 'object' ? response.json() : {} )
-	.then((responseJson)=>{
-		console.log(responseJson.dataset.data);
-		return Correlation.quandl_autocorrelation(responseJson.dataset.data)
-	})
-	.then((results)=>{
-		crashData.three['correlation'] = results;
-		res.json({crashdata: crashData}); 
-	})
-	.catch((err)=>{
-		console.log(err);
-		res.json({error: error});
-		next(error);
-	})  
-});
-**/
-
+//
 app.get('/history', function(req,res){
-
 	let historicalprice = null;
-
 	fetch('https://www.quandl.com/api/v3/datasets/BCHARTS/BITSTAMPUSD.json?api_key=QmmQMekx-LKbhz9eu47w',{
       method: 'get',
       mode: 'cors'
     })
     .then((response) => typeof response == 'object' ? response.json() : {} )
     .then((responseJson)=> {
-
-   
-      // res.json({historical_data: responseJson.dataset.data});
       historicalprice = responseJson.dataset.data;
-      // this.dataFromTSV(history_data);
       return Correlation.quandl_autocorrelation(responseJson.dataset.data);
     })
  	.then((results)=>{
-
- 		// console.log(results); 
-
 		res.json({
 			historical_data: historicalprice,
 			autocorrdata: results
 		}); 
 	})
-
-
-
     .catch((err)=>{
       console.log(err);
       res.json({error: error});
       next(err);
     });
 });
-
-
+//
 app.get('*',(req,res)=>{
 	res.sendFile(path.resolve(__dirname,'./','bundle'));
 });
-
-
+//
 app.listen(app.get('port'), ()=>{
 	console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
--- correlation.js
+++ correlation.js
@@ -2,7 +2,6 @@ let Promise = require('bluebird');
 const autocorrelation = require('./build/Release/autocorrelation');
 const fetch = require('node-fetch');
 
-
 function Correlation(){
 	this.data = {
 		date: null,
@@ -10,96 +9,16 @@ function Correlation(){
 		volume: null
 	};
 }; 
-
-let crashHistory = {
-	'one': 'https://www.quandl.com/api/v3/datasets/BCHARTS/BITSTAMPUSD.json?api_key=oaWPkjrfz_aQmyPmE-WT&start_date=2012-06-25&end_date=2012-09-21', //0->15
-	'two': 'https://www.quandl.com/api/v3/datasets/BCHARTS/BITSTAMPUSD.json?api_key=oaWPkjrfz_aQmyPmE-WT&start_date=2013-02-01&end_date=2013-06-01', //
-	'three': 'https://www.quandl.com/api/v3/datasets/BCHARTS/BITSTAMPUSD.json?api_key=oaWPkjrfz_aQmyPmE-WT&start_date=2013-07-01&end_date=2015-05-01'
-};
-
-let crashData = {
-	'one': {
-		history: null,
-		correlation: null
-	},
-	'two': {
-		history: null,
-		correlation: null
-	},
-	'three': {
-		history: null,
-		correlation: null
-	}
-};
-
-Correlation.prototype.bitcoinaverage_autocorrelation = function(data){
-
-	let object = {	
-		'close': []
-	};
-
-	for(let key in data.bpi){
-		object.close.push(data.bpi[key]);
-	};
-
-	//console.log(this.data.close.length);
-	return new Promise(function(resolve,reject){
-		autocorrelation(object,function(results){
-			resolve(results);
-		});
-	});
-};
-/* gdax data structure
--time bucket start time
--low lowest price during the bucket interval
--high highest price during the bucket interval
--open opening price (first trade) in the bucket interval
--close closing price (last trade) in the bucket interval
--volume volume of trading activity during the bucket interval
-*/
-
-Correlation.prototype.gdax_autocorrelation = function(data){
-
-	let object = {	
-		'close': []
-	};
-
-
-
-	for(let x=0;x<data.length;x++){
-		//console.log(data[x][4]);
-		object.close.push(data[x][4]);
-	};
-
-	return new Promise(function(resolve,reject){
-		autocorrelation(object,function(results){
-			resolve(results);
-		});
-	});
-};
-
+//
 Correlation.prototype.quandl_autocorrelation = function(data){
-	
 	
 	if(data.data!=undefined){
 			let object = {	
 				'close': []
 			};
-
-			// console.log(data.data);
-			/**
-			for(let x=0;x<data.data.length;x++){
-				// object.close.push(data[x][4]); directly from quandl
-				object.close.push(data.data[x].close);
-			};
-			**/
-
-
-
 			for(let x=data.data.length-1; x>=0 ;x--){
 				object.close.push(data.data[x].close);
 			}
-	
 			return new Promise(function(resolve,reject){
 				autocorrelation(object,function(results){
 					resolve(results);
@@ -108,90 +27,8 @@ Correlation.prototype.quandl_autocorrelation = function(data){
 	}
 
 };
-
-
 module.exports = new Correlation();
 
-/**
-let Autocorrelation = new Correlation();
 
-fetch(crashHistory.one,{
-	method: 'get',
-	mode: 'cors'
-})
-.then((response) => typeof response == 'object' ? response.json() : {} )
-.then((responseJson)=>{
-	crashData.one['history'] = responseJson.dataset.data;
-	return Autocorrelation.quandl_autocorrelation(responseJson.dataset.data);
-})
-.then((results)=>{
-	crashData.one['correlation'] = results;
 
-	return fetch(crashHistory.two,{
-		method: 'get',
-		mode: 'cors'
-	})
-})
-.then((response) => typeof response == 'object' ? response.json() : {} )
-.then((responseJson)=>{
-
-	crashData.two['history'] = responseJson.dataset.data;
-	return Autocorrelation.quandl_autocorrelation(responseJson.dataset.data);
-})
-.then((results)=>{
-	// 0->15 crash
-	crashData.two['correlation'] = results;
-	return fetch(crashHistory.three,{
-		method: 'get',
-		mode: 'cors'
-	})
-})
-.then((response)=> typeof response == 'object' ? response.json() : {} )
-.then((responseJson)=>{
-	crashData.three['history'] = responseJson.dataset.data;
-	return Autocorrelation.quandl_autocorrelation(responseJson.dataset.data)
-})
-.then((results)=>{
-	crashData.three['correlation'] = results;
-
-})
-.catch((err)=>{console.log(err)})
-***/
-
-//let url = 'https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=daily&?format=json';
-/**
-let url = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-01-01&end=2017-12-01';
-fetch(url,{
-	method: 'get',
-	mode: 'cors'
-})
-.then((response) => typeof response == 'object' ? response.json() : {} )
-.then((responseJson)=>{
-	return Autocorrelation.bitcoinaverage_autocorrelation(responseJson);
-})
-.then((results) => {
-	console.log(results);
-})
-.catch((err) =>{
-	console.log(err);
-});
-**/
-
-/**gdax
-let url = 'https://api.gdax.com/products/BTC-USD/candles?format(2015-06-01, 2017-06-01,500000)';
-fetch(url,{
-	method: 'get',
-	mode: 'cors'
-})
-.then((response) => typeof response == 'object' ? response.json() : {} )
-.then((responseJson)=>{
-	return Autocorrelation.gdax_autocorrelation(responseJson);
-})
-.then((results) => {
-	// console.log(results);
-})
-.catch((err) =>{
-	console.log(err);
-});
-**/
     
